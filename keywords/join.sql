213f4c39fc552f2991c0396b12cc8242c2941cfb
Preben Andersen <preb1anders1@gmail.com>
2023-05-27 12:23:01 +0200

    hey

--- keywords/join.sql
+++ keywords/join.sql
@@ -3,10 +3,9 @@
 -- Ved å angi hvilke kolonner som skal sammenlignes,
 -- kan du få resultatsettet til å inkludere kun de radene som har matchende verdier i de angitte kolonnen
 
-
-
 -- INNER JOIN
 -- Kombinerer rader fra to tabeller basert på en felles kolonneverdi og returnere alle radene som har denne felles kolonneverdien.
+
 
 -- LEFT JOIN
 -- Returnerer alle rader fra venstre tabell og de matchende radene fra høyre tabell basert på felles kolonneverdier.
@@ -18,7 +17,6 @@
 
 -- FULL JOIN
 -- Returnere alle rader fra begge tabeller, og NULL-verdier for kolonnene som ikke har matchende rader i den andre tabellen.
-
 
 -- CROSS JOIN
 -- Returnere det kartesiske produktet av rader av to tabeller. Altså alle mulige kombinasjoner av rader.
