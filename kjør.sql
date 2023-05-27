bad3caec2aa532ff68f50f33a19868da63c94e86
S669828 <sivertbrattgjerd@gmail.com>
2023-05-27 12:47:41 +0200

    Oppgave2 Eksamen 2022

--- kjør.sql
+++ kjør.sql
@@ -1,1 +1,41 @@
--- Sql syntaks kjør
+--a)
+create table(
+passering_id serial primary key,
+regnr varchar(7) references bil(regnr),
+sted varchar(255) not null,
+tidspunkt datetime not null
+);
+
+--b)
+alter table passering
+rename column sted to bod;
+
+--c)
+select *
+from bil
+left join passering on bil.regnr = passering.regnr;
+
+--d)
+select *
+from bil
+inner join passering on bil.regnr = passering.regnr;
+
+--e)
+-- Bil ⨝_bil.regnr=passering.regnr Passering
+
+--f)
+
+select passering.regnr count(*)as Antall_passeringer 
+from passering group by regnr;
+
+--g)
+select *
+from passering
+where regnr=AA10000
+order by date limit(1) desc;
+
+--h)
+select passering.regnr count(*) as Uregistrert
+from passering
+where regnr is null;
+
