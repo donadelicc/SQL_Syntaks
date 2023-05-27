5410a78c0a64c6a405403f4863040c64b9649430
598128 <skjerdalthomas@gmail.com>
2023-05-27 12:23:25 +0200

    relasjonsalgebra

--- keywords/relasjonslagebra.sql
+++ keywords/relasjonslagebra.sql
@@ -1,13 +1,35 @@
--- σ (Sigma): Brukes til å filtrere rader basert på en betingelse. For eksempel σ(age > 30)(Personer) vil returnere alle personer med alder over 30.
+Sure! Here are some common relational algebra symbols along with their equivalent SQL queries:
 
--- π (Pi): Brukes til å velge spesifikke kolonner fra en tabell. For eksempel π(navn, alder)(Personer) vil returnere bare navn og alder for hver person.
+Selection (σ):
 
--- ⋈ (Join): Brukes til å kombinere rader fra to tabeller basert på en felles kolonneverdi. Det finnes forskjellige typer join-operasjoner, for eksempel θ-join (bruker en betingelse), equijoin (bruker en likhetsbetingelse), natural join (bruker felleskolonner med samme navn), og outer join (beholder ikke-matchende rader).
+Relational Algebra: σ_condition(R)
+SQL Equivalent: SELECT * FROM R WHERE condition
+Projection (π):
 
--- ∪ (Union): Brukes til å kombinere rader fra to tabeller med samme kolonneoppsett. Resultatet vil være en tabell som inneholder unike rader fra begge tabellene.
+Relational Algebra: π_attributes(R)
+SQL Equivalent: SELECT attributes FROM R
+Union (⋃):
 
--- ∩ (Intersection): Brukes til å finne felles rader mellom to tabeller med samme kolonneoppsett. Resultatet vil være en tabell som inneholder rader som finnes i begge tabellene.
+Relational Algebra: R ⋃ S
+SQL Equivalent: SELECT * FROM R UNION SELECT * FROM S
+Intersection (⋂):
 
--- - (Difference): Brukes til å finne forskjellen mellom rader i to tabeller med samme kolonneoppsett. Resultatet vil være en tabell som inneholder rader som finnes i den første tabellen, men ikke i den andre.
+Relational Algebra: R ⋂ S
+SQL Equivalent: SELECT * FROM R INTERSECT SELECT * FROM S
+Difference (−):
 
--- × (Cartesian Product): Brukes til å generere et kartesisk produkt av rader fra to tabeller. Resultatet vil være en tabell som inneholder alle mulige kombinasjoner av rader fra begge tabellene.
+Relational Algebra: R − S
+SQL Equivalent: SELECT * FROM R EXCEPT SELECT * FROM S
+Cartesian Product (×):
+
+Relational Algebra: R × S
+SQL Equivalent: SELECT * FROM R, S
+Join (⨝):
+
+Relational Algebra: R ⨝_condition S
+SQL Equivalent: SELECT * FROM R INNER JOIN S ON condition
+Division (÷):
+
+Relational Algebra: R ÷ S
+SQL Equivalent: SELECT * FROM R WHERE NOT EXISTS (SELECT * FROM S MINUS SELECT * FROM R)
+These examples demonstrate the correspondence between relational algebra operations and their SQL counterparts. Keep in mind that the specific SQL syntax may vary slightly depending on the database management system (DBMS) you are using.
