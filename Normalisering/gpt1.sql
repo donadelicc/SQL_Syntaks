75431a212944aad181e1a14b83efd53789b232e2
Preben Andersen <preb1anders1@gmail.com>
2023-05-29 11:55:22 +0200

    hey

--- Normalisering/gpt1.sql
+++ Normalisering/gpt1.sql
@@ -7,3 +7,14 @@ Lagersted,
 Lagerhylle
 )
 
+/*
+a) Identifiser primærnøkkelen for tabellen. Du må opprette en sammensatt primærnøkkel som består av én eller flere attributter i tabellen.
+
+Analyser tabellen og identifiser funksjonelle avhengigheter. Beskriv hvilke attributter som er funksjonelt avhengige av primærnøkkelen, og hvilke som ikke er.
+
+Normaliser tabellen til andre normalform (2NF). Opprett nye tabeller hvis nødvendig for å eliminere delvis funksjonelle avhengigheter.
+
+Normaliser tabellen videre til tredje normalform (3NF). Opprett nye tabeller hvis nødvendig for å eliminere transitiv funksjonell avhengighet
+*/
+
+
